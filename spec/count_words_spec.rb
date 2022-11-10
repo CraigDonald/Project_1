0107bfd6d516845505528484ad79d069a3e67f13
Craig Donald <1craig.donald@gmail.com>
2022-11-10 12:08:53 +0000

    String length tests all pass

--- spec/count_words_spec.rb
+++ spec/count_words_spec.rb
@@ -13,6 +13,9 @@ RSpec.describe CountWords do
         it "If string is populated, return number of words in that string" do
             count = CountWords.new
             expect(count.count_words("String goes here")).to eq 3
+            expect(count.count_words("One")).to eq 1
+            expect(count.count_words("This string is really very ridiculously long who talks like this honestly?")).to eq 12
         end
+    
     
 end
