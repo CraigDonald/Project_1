94edaa66dfa4c7073c574e11490e1aa48638221f
Craig Donald <1craig.donald@gmail.com>
2022-11-10 14:54:45 +0000

    Working string checker

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -14,19 +14,15 @@ RSpec.describe ToDo do
         it "Has a string containing '#TODO'" do
             example2 = ToDo.new
                 expect(example2.todo_check("#TODO Check heating is off")).to eq true
+                expect(example2.todo_check("Check cooker is off #TODO")).to eq true
+                expect(example2.todo_check("Check this #TODO later.")).to eq true
         end
     
     context "Given a string"
         it "Has a string, but does not contain '#TODO'" do
             example3 = ToDo.new
                 expect(example3.todo_check("Get washing in")).to eq false
+                expect(example3.todo_check("Call friend later")).to eq false
+                expect(example3.todo_check("Ask to finish early")).to eq false
         end
 end
-
-    #    it "Has '#TODO' within it somewhere"
-    
-    #end
-
-    #    it "Doesn't have '#TODO' within it"
-    
-    #end
